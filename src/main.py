f910308821bb9266861e76a1f81a4d01bcbe9aef
Valentin Lopez <valentinlopezandres@gmail.com>
2024-02-25 20:24:56 -0300

    Add kmeans route

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_restful import Api
 
 from routes.knn_classifier import KNNClassifierRoute
+from routes.kmeans_classifier import KMeansClassifierRoute
 
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(KNNClassifierRoute, '/knn_classifier')
+api.add_resource(KMeansClassifierRoute, '/kmeans_classifier')
 
 @app.route("/")
 def hello_world():
